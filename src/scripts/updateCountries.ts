3b5fff9f470d3d8637b936ebe88ab9105f4dc1b6
Timur Oberhuber <19150752+Timur-O@users.noreply.github.com>
2023-08-05 00:45:07 +0200

    feat: :sparkles: Add Creation of User Document

---
+++ src/scripts/updateCountries.ts
@@ -0,0 +1,70 @@
+// Use Chrome Like So:
+// open -n -a /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --args --user-data-dir="/tmp/chrome_dev_test" --disable-web-security
+
+export default async function updateCountries() {
+    let wikipediaSovereignCountries: string;
+    let unCountries: Array<any> = [];
+    let nonUnSovereignCountries: Array<any> = [];
+    let nonUnNonSovereignCountries: Array<any> = [];
+
+    await fetch("https://en.wikipedia.org/wiki/List_of_sovereign_states")
+        .then(response => {
+            response.text().then(data => {
+                wikipediaSovereignCountries = data;
+            });
+        }).catch(error => {
+            console.error("Error updating countries:", error);
+        });
+
+    await fetch("https://raw.githubusercontent.com/mledoze/countries/master/countries.json")
+        .then(response => {
+            response.json().then(data => {
+                data.forEach((country: any) => {
+                    if (country.unMember) {
+                        unCountries.push(country);
+                    } else {
+                        const formattedName: string = country.name.common.replaceAll(" ", "_");
+                        if (wikipediaSovereignCountries.indexOf('<span id="' + formattedName + '"></span>') != -1) {
+                            nonUnSovereignCountries.push(country);
+                        } else {
+                            nonUnNonSovereignCountries.push(country);
+                        }
+                    }
+                });
+
+                unCountries = unCountries.reduce((acc, country) => {
+                    const region = country.region;
+                    if (!acc[region]) {
+                        acc[region] = [];
+                    }
+                    acc[region].push(country);
+                    return acc;
+                }, {});
+
+                nonUnSovereignCountries = nonUnSovereignCountries.reduce((acc, country) => {
+                    const region = country.region;
+                    if (!acc[region]) {
+                        acc[region] = [];
+                    }
+                    acc[region].push(country);
+                    return acc;
+                }, {});
+
+                nonUnNonSovereignCountries = nonUnNonSovereignCountries.reduce((acc, country) => {
+                    const region = country.region;
+                    if (!acc[region]) {
+                        acc[region] = [];
+                    }
+                    acc[region].push(country);
+                    return acc;
+                }, {});
+
+                // Print the data
+                console.log("UN Member Countries:", JSON.stringify(unCountries));
+                console.log("Non-UN Sovereign Countries:", JSON.stringify(nonUnSovereignCountries));
+                console.log("Non-UN Non-Sovereign Countries:", JSON.stringify(nonUnNonSovereignCountries));
+            });
+        }).catch(error => {
+            console.error("Error updating countries:", error)
+        });
+}
--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -1,49 +1,103 @@
 <template>
   <ion-page>
     <ion-content :color="'primary'">
-      <div class="container">
+      <div id="container-top">
         <ion-thumbnail class="app-icon">
           <img src="/favicon.png"  alt="App Icon"/>
         </ion-thumbnail>
         <h1>Travel Buddy</h1>
+      </div>
+      <div id="container-bottom">
         <ion-img src="/google_login.png" @click="loginWithGoogle"></ion-img>
-        <ion-text color="danger" v-if="error">Something went wrong, please try again!</ion-text>
+        <ion-text class="error" color="danger" v-if="error">Something went wrong, please try again!</ion-text>
       </div>
     </ion-content>
   </ion-page>
 </template>
 
 <script setup lang="ts">
-import { IonPage, IonContent, IonThumbnail, IonImg } from "@ionic/vue";
+import { IonPage, IonContent, IonThumbnail, IonImg, IonText } from "@ionic/vue";
 import { signInWithPopup } from 'firebase/auth'
-import { googleAuthProvider } from "@/firebase";
-import {useFirebaseAuth} from "vuefire";
+import { db, getCountriesInfo, getUserInfo, googleAuthProvider } from "@/firebase";
+import { getCurrentUser, useFirebaseAuth } from "vuefire";
 import router from "@/router";
+import { doc, setDoc } from "firebase/firestore";
+import { ref } from "vue";
 
 const auth = useFirebaseAuth();
-let error = false;
+const error = ref(false);
 
 function loginWithGoogle() {
-  signInWithPopup(auth!, googleAuthProvider).then(() => {
+  signInWithPopup(auth!, googleAuthProvider).then(async () => {
+    const currentUser = await getCurrentUser();
+    const userInfo = await getUserInfo(currentUser?.uid!);
+
+    // Create document for user if it doesn't exist
+    if (userInfo == null) {
+      const countriesInfo = await getCountriesInfo();
+
+      const unCountries = Object.values(JSON.parse(countriesInfo.un)).flatMap((region) => {
+        return region.map((country) => {
+          return {
+            code: country.cca3,
+            visited: false
+          };
+        });
+      });
+      const nonUnCountries = Object.values(JSON.parse(countriesInfo.nonUn)).flatMap((region) => {
+        return region.map((country) => {
+          return {
+            code: country.cca3,
+            visited: false
+          };
+        });
+      });
+      const nonSovereignCountries = Object.values(JSON.parse(countriesInfo.nonSovereign)).flatMap((region) => {
+        return region.map((country) => {
+          return {
+            code: country.cca3,
+            visited: false
+          };
+        });
+      });
+
+      await setDoc(doc(db, "users", currentUser?.uid), {
+        name: currentUser?.displayName,
+        email: currentUser?.email,
+        countries: {
+          un: unCountries,
+          nonUn: nonUnCountries,
+          nonSovereign: nonSovereignCountries
+        }
+      });
+    }
+
     router.push(router.currentRoute.value.query.redirect || '/');
   }).catch((reason) => {
-    error = true;
+    error.value = true;
     console.error('Failed to login:', reason);
   })
 }
 </script>
 
 <style scoped>
-.container {
+#container-top, #container-bottom {
   text-align: center;
   position: absolute;
   left: 0;
   right: 0;
-  top: 40%;
   transform: translateY(-50%);
   display: flex;
   flex-direction: column;
   align-items: center;
+}
+
+#container-top {
+  top: 35%;
+}
+
+#container-bottom {
+  top: 60%;
 }
 
 .app-icon {
@@ -53,5 +107,9 @@ function loginWithGoogle() {
 
 .app-icon > img {
   border-radius: 5px;
+}
+
+.error {
+  margin-top: 2em;
 }
 </style>
