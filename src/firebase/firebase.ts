2b3c6c861a94d8d58a91fdcd8583dfe03f176bea
Timur O <19150752+Timur-O@users.noreply.github.com>
2023-10-30 16:36:20 +0800

    feat: :ok_hand: Code Improvements

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -1,9 +1,6 @@
 import {initializeApp} from 'firebase/app';
 import {GoogleAuthProvider} from 'firebase/auth';
-import {collection, doc, getFirestore} from 'firebase/firestore'
-import {useDocument} from "vuefire";
-import CountriesInfo from "@/models/CountriesInfo";
-import UserInfo from "@/models/UserInfo";
+import {collection, getFirestore} from 'firebase/firestore'
 import secret from './secret.json';
 import {FirebaseOptions} from "@firebase/app";
 
