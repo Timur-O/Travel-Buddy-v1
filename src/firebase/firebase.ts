81295759ce8028ba75b7c61e11dccd0bd12168be
Timur O <19150752+Timur-O@users.noreply.github.com>
2024-06-14 15:04:18 +0200

    :bug: Final Pipeline Fix

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -4,9 +4,6 @@ import {collection, getFirestore} from 'firebase/firestore'
 import {FirebaseOptions} from "@firebase/app";
 import { initializeAppCheck, ReCaptchaEnterpriseProvider } from "firebase/app-check";
 
-console.log(typeof process)
-console.log(process.env.FIREBASE_SECRET_JSON)
-
 const secretJson = (typeof process !== 'undefined') && process.env.FIREBASE_SECRET_JSON ?
     JSON.parse(process.env.FIREBASE_SECRET_JSON) :
     await import('./secret.json');
