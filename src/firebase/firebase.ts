33be70180902e3d8aabd072c16da047b84835442
Timur O <19150752+Timur-O@users.noreply.github.com>
2024-06-14 13:23:57 +0200

    :sparkles: Add Firebase Hosting

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -2,9 +2,16 @@ import {initializeApp} from 'firebase/app';
 import {GoogleAuthProvider} from 'firebase/auth';
 import {collection, getFirestore} from 'firebase/firestore'
 import secret from './secret.json';
+import keys from './keys.json';
 import {FirebaseOptions} from "@firebase/app";
+import { initializeAppCheck, ReCaptchaEnterpriseProvider } from "firebase/app-check";
 
 export const firebaseApp = initializeApp(secret as FirebaseOptions);
+
+const appCheck = initializeAppCheck(firebaseApp, {
+    provider: new ReCaptchaEnterpriseProvider(keys.appCheck.siteKey),
+    isTokenAutoRefreshEnabled: true
+});
 
 export const googleAuthProvider = new GoogleAuthProvider();
 export const db = getFirestore(firebaseApp);
