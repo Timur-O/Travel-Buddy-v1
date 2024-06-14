984153aecafddbff05615663a309e3836d991196
Timur O <19150752+Timur-O@users.noreply.github.com>
2024-06-14 14:53:09 +0200

    :bug: Fix Pipeline Locally (again)

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -4,15 +4,13 @@ import {collection, getFirestore} from 'firebase/firestore'
 import {FirebaseOptions} from "@firebase/app";
 import { initializeAppCheck, ReCaptchaEnterpriseProvider } from "firebase/app-check";
 
-let secretJson;
-let keysJson;
-try {
-    secretJson = JSON.parse(process.env.FIREBASE_SECRET_JSON!);
-    keysJson = JSON.parse(process.env.FIREBASE_KEYS_JSON!);
-} catch (e) {
-    secretJson = await import('./secret.json');
-    keysJson = await import('./keys.json');
-}
+const secretJson = (typeof process !== 'undefined') && process.env.FIREBASE_SECRET_JSON ?
+    JSON.parse(process.env.FIREBASE_SECRET_JSON) :
+    await import('./secret.json');
+
+const keysJson = (typeof process !== 'undefined') && process.env.FIREBASE_KEYS_JSON ?
+    JSON.parse(process.env.FIREBASE_KEYS_JSON) :
+    await import('./keys.json');
 
 export const firebaseApp = initializeApp(secretJson as FirebaseOptions);
 
